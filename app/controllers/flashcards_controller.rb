410bbb3e0b99c50d1c994115e926154a869ef29b
Eric Snyder <ersnyder1@gmail.com>
2013-11-10 17:06:00 -0600

    Flashcards
    Couldn't figure out the final piece (how to get the app to sequence
    through the students vs. just showing the first student 29 times); so I
    modified the app to be something that I figured would also be useful for
    learning names. Each refresh of the page generates a random student.
    Clicking 'Show another student' refreshes the page.

--- app/controllers/flashcards_controller.rb
+++ app/controllers/flashcards_controller.rb
@@ -3,9 +3,16 @@ require 'json'
 
 class FlashcardsController < ApplicationController
   def index
-    # url = ?
-    # raw_data = ?
+  
+    url = "http://kiei925.herokuapp.com/students.json?access_token=broncos"
+    raw_data = open(url).read
+    parsed_data = JSON.parse(raw_data)
+    @students = parsed_data
+ 	
+    @id= rand(0..28)
+    @first_name = parsed_data[@id]["first_name"]
+    @last_name = parsed_data[@id]["last_name"]
+    @image_url = parsed_data[@id]["image_url"]
 
-    # @students = ?
   end
 end
